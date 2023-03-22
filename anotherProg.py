47f8ca4ee82e0be375db9c94f226f1514b3e3f5a
AymenEssid1 <aymen.essid@esprit.tn>
2023-03-22 16:29:46 +0100

    update : added new function in anotherProg

--- anotherProg.py
+++ anotherProg.py
@@ -1,5 +1,19 @@
+import random
+
+number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess < number:
+        print("Too low, try again!")
+    elif guess > number:
+        print("Too high, try again!")
+    else:
+        print("Congratulations, you guessed the number!")
+        break
+
 for i in range(10):
 
-    print(i+1)
+    print(i+2)
 
-    print(i,"hello")
+    print(i+3,"hello")
