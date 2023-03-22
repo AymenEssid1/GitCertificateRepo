e707916706a0c1544522f2cf33d1287c5b8bca28
AymenEssid1 <aymen.essid@esprit.tn>
2023-03-22 10:26:23 +0100

    add : basic python program

---
+++ simpleProgram.py
@@ -0,0 +1,12 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+# iterate through all numbers up to 1000 and print the prime ones
+for i in range(2, 1001):
+    if is_prime(i):
+        print(i)
