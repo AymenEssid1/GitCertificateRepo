b5c5a1b2dc4943e1a040b8012b2fa5a0861f44f3
AymenEssid1 <92875555+AymenEssid1@users.noreply.github.com>
2023-03-22 16:02:07 +0100

    Update simpleProgram.py

--- simpleProgram.py
+++ simpleProgram.py
@@ -1,16 +1,4 @@
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
-
-# iterate through all numbers up to 1000 and print the prime ones
-for i in range(2, 1001):
-    if is_prime(i):
-        print(i+1)
 
 
-print("THIS PROGRAM DISPLAYS ALL PRIME NUMBERS UP TO 1000")
+print("THIS is a third branch")
 print("done")
